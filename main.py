0bbfa87deefc63c2ecfbcb3de32e392ca368c8fa
Sumafix <gzadkowski@gmail.com>
2021-03-08 20:08:24 +0100

    Added function check_money() and calc_change. Refactored main part of program.

--- main.py
+++ main.py
@@ -32,14 +32,14 @@ resources = {
 
 possible_commands = ["off", "report", "espresso", "latte", "cappuccino"]
 is_machine_work = True
-money = 0.0
+profit = 0.0
 
 
 def report_machine():
     water = resources['water']
     milk = resources['milk']
     coffee = resources['coffee']
-    return f"Water: {water}ml\nMilk: {milk}ml\nCoffee: {coffee}g\nMoney: ${round(money, 2)}"
+    return f"Water: {water}ml\nMilk: {milk}ml\nCoffee: {coffee}g\nMoney: ${round(profit, 2)}"
 
 
 def check_resources(drink_name):
@@ -51,35 +51,42 @@ def check_resources(drink_name):
     return True
 
 
+def check_money(coins, drink_name):
+    if coins >= MENU[drink_name]['cost']:
+        return True
+    print("Sorry that's not enough money. Money refunded.")
+    return False
+
+
 def sum_coins(num_of_quarters, num_of_dimes, num_of_nickles, num_of_pennies):
     return round(0.25 * num_of_quarters + 0.1 * num_of_dimes + 0.05 * num_of_nickles + 0.01 * num_of_pennies, 2)
 
 
+def calc_change(coins, drink_name):
+    return round(coins - MENU[drink_name]['cost'], 2)
+
+
 while is_machine_work:
-    command = ""
-    while command not in possible_commands:
-        command = input("   What would you like? (espresso/latte/cappuccino): ").lower()
-    if command == "off":
+    choice = ""
+    while choice not in possible_commands:
+        choice = input("   What would you like? (espresso/latte/cappuccino): ").lower()
+    if choice == "off":
         is_machine_work = False
-    elif command == "report":
+    elif choice == "report":
         print(report_machine())
     else:
-        if check_resources(command):
+        if check_resources(choice):
             print("Please insert coins.")
             quarters = int(input("how many quarters?: "))
             dimes = int(input("how many dimes?: "))
             nickles = int(input("how many nickles?: "))
             pennies = int(input("how many pennies?: "))
             payment = sum_coins(quarters, dimes, nickles, pennies)
-            cost = MENU[command]['cost']
-            if payment > cost:
-                change = round(payment - cost, 2)
-                print(f"Here is ${change} in change.")
-                resources['water'] -= MENU[command]['ingredients']['water']
-                if 'milk' in MENU[command]['ingredients']:
-                    resources['milk'] -= MENU[command]['ingredients']['milk']
-                resources['coffee'] -= MENU[command]['ingredients']['coffee']
-                money += cost
-                print(f"Here is your {command} ☕. Enjoy!")
-            else:
-                print("Sorry that's not enough money. Money refunded.")
+            if check_money(payment, choice):
+                print(f"Here is ${calc_change(payment, choice)} in change.")
+                resources['water'] -= MENU[choice]['ingredients']['water']
+                if 'milk' in MENU[choice]['ingredients']:
+                    resources['milk'] -= MENU[choice]['ingredients']['milk']
+                resources['coffee'] -= MENU[choice]['ingredients']['coffee']
+                profit += MENU[choice]['cost']
+                print(f"Here is your {choice} ☕. Enjoy!")
